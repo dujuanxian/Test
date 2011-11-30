0d226955d88e1e307978772e33330e8c3eab8cf2
Du Juan <dujuanxian@yahoo.cn>
2011-11-30 18:37:26 +0800

    Initial version of class one.

---
+++ src/ClassOne.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-11-30
+ * Time: 下午6:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassOne {
+    public static void main(String[] args){
+        System.out.println("Class One");
+    }
+}
