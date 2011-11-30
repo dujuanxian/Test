ab887d67815b3804b86711d389205fc327f4baa6
Jim <jim@yahoo.cn>
2011-11-30 19:53:21 +0800

    Class Two is added

---
+++ src/ClassTwo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-11-30
+ * Time: 下午7:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassTwo {
+    public static void main(String[] args){
+        System.out.println("Class Two");
+    }
+}
